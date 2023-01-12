c009d012cd218f29b6a7e000e6e4394773926af5
haftamudesta <desta451616@hotmail.com>
2023-01-13 00:16:04 +0300

    Modify Awesome books complete

--- script.js
+++ script.js
@@ -67,13 +67,12 @@ class Books {
         this.StoreBooks.push(this.BookList);
         localStorage.setItem('BookList', JSON.stringify(this.StoreBooks));
         this.DisplayAllBooks();
-
-        this.NewTitle.value = 'Title';
-        this.NewAuthor.value = 'Author';
       } else {
         this.ErrorChecker.textContent = 'Field must contain data!';
         this.ErrorChecker.style.color = 'red';
       }
+      this.NewTitle.value = '';
+      this.NewAuthor.value = '';
     });
   };
 
@@ -86,6 +85,7 @@ class Books {
             if (index !== Findid) {
               return Book;
             }
+            window.location.reload();
           });
           localStorage.setItem('BookList', JSON.stringify(DeleteBook));
           this.DecideOnDisplay();
@@ -95,6 +95,106 @@ class Books {
   };
 }
 const books = new Books();
-books.DisplayAllBooks();
-books.AddNewBook();
-books.RemoveBooks();
+//books.DisplayAllBooks();
+//books.AddNewBook();
+//books.RemoveBooks();
+
+const fullTime = document.querySelector('.time');
+const dates = () => {
+  const time = new Date();
+  const year = time.getFullYear();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  const second = time.getSeconds();
+  let monthValue;
+  switch (month) {
+    case 0:
+      monthValue = 'January';
+      break;
+    case 1:
+      monthValue = 'February';
+      break;
+    case 2:
+      monthValue = 'March';
+      break;
+    case 3:
+      monthValue = 'April';
+      break;
+    case 4:
+      monthValue = 'May';
+      break;
+    case 5:
+      monthValue = 'June';
+      break;
+    case 6:
+      monthValue = 'July';
+      break;
+    case 7:
+      monthValue = 'August';
+      break;
+    case 9:
+      monthValue = 'September';
+      break;
+    case 10:
+      monthValue = 'October';
+      break;
+    case 11:
+      monthValue = 'November';
+      break;
+    case 12:
+      monthValue = 'December';
+      break;
+    default:
+      break;
+  }
+  fullTime.textContent = `${monthValue} ${date}th ${year},${hour}:${minute}:${second}`;
+};
+
+const DisplayBooks = document.querySelector('.List-Items');
+const AddBooks = document.querySelector('.add-books');
+const Contact = document.querySelector('.Contact');
+const List = document.querySelector('#List');
+const Add = document.querySelector('#Add');
+const Link = document.querySelector('#Link');
+const DisplayAllBooks = () => {
+  List.addEventListener('click', () => {
+    DisplayBooks.classList.add('active');
+    List.style.color = 'rgb(255, 0, 0)';
+    Link.style.color = 'black';
+    Add.style.color = 'black';
+    Contact.classList.remove('active');
+    AddBooks.classList.remove('active');
+    DisplayBooks.style.display = 'block';
+    books.RemoveBooks();
+  });
+};
+
+const AddNewBook = () => {
+  Add.addEventListener('click', () => {
+    AddBooks.classList.add('active');
+    Add.style.color = 'rgb(255, 0, 0)';
+    Link.style.color = 'black';
+    List.style.color = 'black';
+    Contact.classList.remove('active');
+    DisplayBooks.classList.remove('active');
+    DisplayBooks.style.display = 'none';
+    books.AddNewBook();
+  });
+};
+const ShowContactDetails = () => {
+  Link.addEventListener('click', () => {
+    Contact.classList.toggle('active');
+    Link.style.color = 'rgb(255, 0, 0)';
+    Add.style.color = 'black';
+    List.style.color = 'black';
+    AddBooks.classList.remove('active');
+    DisplayBooks.classList.remove('active');
+    DisplayBooks.style.display = 'none';
+  });
+};
+DisplayAllBooks();
+AddNewBook();
+ShowContactDetails();
+dates();
