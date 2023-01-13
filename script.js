21fb6d1467eb75a30e752fc3f97eb64999bbb27c
Uthmanbello <uthmanbello88@gmail.com>
2023-01-13 16:09:58 +0100

    Edit pages

--- script.js
+++ script.js
@@ -26,29 +26,21 @@ class Books {
   DisplayAllBooks = () => {
     this.DisplayBooks.innerHTML = '';
     this.HoldBooks = JSON.parse(localStorage.getItem('BookList'));
-    if (this.HoldBooks != null) {
+    if (this.HoldBooks !== null) {
       this.StoreBooks = this.HoldBooks;
       this.HoldBooks.forEach((Books, index) => {
         this.DisplayBooks.innerHTML += `
          <tr class="Display" >
          <td><span>"</span>${Books.Title}<span>" by </span>${Books.Author}</td>
-         <td><button type="button" class="remove" id="${index}">Remove</button></td>
+         <td><button class="remove" id="${index}">Remove</button></td>
          </tr>`;
       });
     }
   };
 
   DecideOnDisplay = () => {
-    if (this.DisplayBooks.innerHTML === '') {
-      this.HoldBooks = JSON.parse(localStorage.getItem('BookList'));
-      this.StoreBooks = this.HoldBooks;
-      this.HoldBooks.forEach((Books, index) => {
-        this.DisplayBooks.innerHTML += `
-         <tr class="Display">
-         <td><span>"</span>${Books.Title}<span>" by </span>${Books.Author}</td>
-         <td><button type="button" class="remove" id="${index}">Remove</button></td>
-         </tr>`;
-      });
+    if (this.SaveBooks === null) {
+      this.DisplayBooks.innerHTML = '';
     } else {
       this.DisplayAllBooks();
     }
@@ -56,9 +48,6 @@ class Books {
 
   AddNewBook = () => {
     this.AddBook.addEventListener('click', () => {
-      if (this.SaveBooks != null) {
-        [];
-      }
       if (this.Validate()) {
         this.BookList = {
           Title: this.NewTitle.value,
@@ -66,13 +55,10 @@ class Books {
         };
         this.StoreBooks.push(this.BookList);
         localStorage.setItem('BookList', JSON.stringify(this.StoreBooks));
-        this.DisplayAllBooks();
-
-        this.NewTitle.value = 'Title';
-        this.NewAuthor.value = 'Author';
+        this.ClearFields();
+        this.DecideOnDisplay();
       } else {
-        this.ErrorChecker.textContent = 'Field must contain data!';
-        this.ErrorChecker.style.color = 'red';
+        this.ErrorChecker.textContent = 'Please fill in all fields';
       }
     });
   };
@@ -89,12 +75,118 @@ class Books {
           });
           localStorage.setItem('BookList', JSON.stringify(DeleteBook));
           this.DecideOnDisplay();
-        });
+         });
       });
     }
   };
 }
+
 const books = new Books();
-books.DisplayAllBooks();
 books.AddNewBook();
 books.RemoveBooks();
+
+//===========================
+//   Single page code
+//===========================
+
+const DisplayBooks = document.querySelector('.List-Items');
+const AddBooks = document.querySelector('.add-books');
+const Contact = document.querySelector('.Contact');
+const List = document.querySelector('#List');
+const Add = document.querySelector('#Add');
+const Link = document.querySelector('#Link');
+const fullTime = document.querySelector('.time');
+
+const DisplayAllBooks = () => {
+  List.addEventListener('click', () => {
+    DisplayBooks.classList.add('active');
+    List.style.color = 'rgb(255, 0, 0)';
+    Link.style.color = 'black';
+    Add.style.color = 'black';
+    Contact.classList.remove('active');
+    AddBooks.classList.remove('active');
+    DisplayBooks.style.display = 'block';
+    books.RemoveBooks();
+  });
+};
+
+const AddNewBook = () => {
+  Add.addEventListener('click', () => {
+    AddBooks.classList.add('active');
+    Add.style.color = 'rgb(255, 0, 0)';
+    Link.style.color = 'black';
+    List.style.color = 'black';
+    Contact.classList.remove('active');
+    DisplayBooks.classList.remove('active');
+    DisplayBooks.style.display = 'none';
+  });
+};
+
+const ShowContactDetails = () => {
+  Link.addEventListener('click', () => {
+    Contact.classList.add('active');
+    Link.style.color = 'rgb(255, 0, 0)';
+    Add.style.color = 'black';
+    List.style.color = 'black';
+    AddBooks.classList.remove('active');
+    DisplayBooks.classList.remove('active');
+    DisplayBooks.style.display = 'none';
+  });
+};
+
+const dates = () => {
+  const time = new Date();
+  const year = time.getFullYear();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  const second = time.getSeconds();
+  let monthValue;
+  switch (month) {
+    case 0:
+      monthValue = 'January';
+      break;
+    case 1:
+      monthValue = 'February';
+      break;
+    case 2:
+      monthValue = 'March';
+      break;
+    case 3:
+      monthValue = 'April';
+      break;
+    case 4:
+      monthValue = 'May';
+      break;
+    case 5:
+      monthValue = 'June';
+      break;
+    case 6:
+      monthValue = 'July';
+      break;
+    case 7:
+      monthValue = 'August';
+      break;
+    case 9:
+      monthValue = 'September';
+      break;
+    case 10:
+      monthValue = 'October';
+      break;
+    case 11:
+      monthValue = 'November';
+      break;
+    case 12:
+      monthValue = 'December';
+      break;
+    default:
+      break;
+  }
+  fullTime.textContent = `${monthValue} ${date}th ${year},${hour}:${minute}:${second}`;
+};
+
+DisplayAllBooks();
+AddNewBook();
+ShowContactDetails();
+dates();
